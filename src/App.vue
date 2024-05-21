efc800830abdac532aa76c5089d46286c682a036
Nguyễn Văn Long <longnv@langtech.vn>
2024-05-21 17:06:08 +0700

    test auto deploy

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
-    <div class="container">
-        <h1>Test tiep nao ok o rfsf dahsdadasgdhsa asdasd asd adsad</h1>
+    <div class="container mt-4">
+        <table class="table table-stripered table-border table-hover">
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th>age</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item,index) in data" :key="index">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.age }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { onMounted, ref } from "vue";
+import axios  from "axios";
 
+const data = ref(null);
+const url = ref(import.meta.env.VITE_APP_API);
+const fetchData = async() => {
+    let response = await axios.get(url.value + 'employee')
+    if(response.status)
+    {
+        data.value = response.data.data;
+    }
 }
+
+onMounted(() => {
+    fetchData();
+})
 </script>
 
 <style>
